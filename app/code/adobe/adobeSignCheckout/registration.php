e999f0cc4f1b15246a6ebb6d8d8f1911f9f6608f
manuelmattar <manuelmattar@wolfsellers.com>
2022-02-28 14:12:39 -0600

    Mega Menu

--- app/code/adobe/adobeSignCheckout/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-$registrar = new ComponentRegistrar();
-
-if ($registrar->getPath(ComponentRegistrar::MODULE, 'adobe_adobeSignCheckout') === null) {
-    ComponentRegistrar::register(ComponentRegistrar::MODULE, 'adobe_adobeSignCheckout', __DIR__);
-}
