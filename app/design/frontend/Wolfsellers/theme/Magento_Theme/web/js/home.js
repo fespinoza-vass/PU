1f21b6a67d61a134eb199fd95e40486ba8900fcb
Itzel Cruz <alejandracruz@wolfsellers.com>
2022-03-22 18:05:20 -0600

    Estilos en cookies y ajustes

--- app/design/frontend/Wolfsellers/theme/Magento_Theme/web/js/home.js
+++ app/design/frontend/Wolfsellers/theme/Magento_Theme/web/js/home.js
@@ -37,4 +37,47 @@ require([
 		slidesToScroll: 1
 	});
 
-});	
+    $('.content-carousel-releases, content-carousel-category').slick({
+        function (config, element) {
+            if (!$(element).hasClass("slick-initialized")) {
+                $(element).slick({
+                    infinite: false,
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                    arrows: true,
+                    dots: true,
+                    responsive: [{
+                        breakpoint: 1025,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 4
+                        }
+                    },
+                        {
+                            breakpoint: 769,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 3
+                            }
+                        },
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 2
+                            }
+                        },
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 2
+                            }
+                        }
+                    ]
+                });
+            }
+        }
+    });
+
+});
