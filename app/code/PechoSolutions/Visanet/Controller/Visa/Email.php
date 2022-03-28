e7d1d50000112b4b9475362341ae5a560cbb9ab6
manuelmattar <manuelmattar@wolfsellers.com>
2022-03-28 10:58:01 -0600

    VisaNet de Ishop

---
+++ app/code/PechoSolutions/Visanet/Controller/Visa/Email.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+    Este controller sera el responsable de conectar con Visanet para generar el token
+    con el precio total del carrito, osea carrito + shipping
+*/
+
+namespace PechoSolutions\Visanet\Controller\Visa;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Store\Model\ScopeInterface;
+use PechoSolutions\Visanet\Model\Library\Visanet;
+   
+class Email extends \Magento\Framework\App\Action\Action {
+    
+    protected $config;
+    private $encryptor;   
+    protected $checkoutSession;
+    protected $resultJsonFactory;   
+    
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,      
+        ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\Encryption\EncryptorInterface $encryptor,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+    )
+    {
+        parent::__construct($context);    
+        $this->scopeConfig = $scopeConfig;
+        $this->encryptor = $encryptor;
+        $this->checkoutSession = $checkoutSession;
+        $this->resultJsonFactory = $resultJsonFactory;
+      
+    }
+ 
+
+
+    public function execute() {         
+        $response = new \Magento\Framework\DataObject(); 
+        if (isset($_POST['email'])){
+            $this->checkoutSession->setGuestEmail($_POST['email']);
+            $response->setEmail(true);
+        }else{
+            $response->setEmail(false);    
+        }
+        return $this->resultJsonFactory->create()
+                                       ->setJsonData($response->toJson());
+
+    }
+}
