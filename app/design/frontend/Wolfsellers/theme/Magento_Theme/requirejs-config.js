ea9a483627b13e0dc10c74b3625233df61ec06f3
irving hernandez <irvinghernandez@wolfsellers.com>
2022-10-21 21:29:31 -0500

    Revert "Merged in PUSOP-21 (pull request #339)"
    This reverts commit 1d1c10339b3366db8c91538cf4765094fbade37d, reversing
    changes made to b13f10cb9293a65b5a507972ec50edd6cdce5e54.

--- app/design/frontend/Wolfsellers/theme/Magento_Theme/requirejs-config.js
+++ app/design/frontend/Wolfsellers/theme/Magento_Theme/requirejs-config.js
@@ -4,7 +4,6 @@ var config = {
         'Magento_Theme/js/login-dropdown',
         'Magento_Theme/js/cart-related',
     	'Magento_Theme/js/home',
-        'Magento_Theme/js/popup-home',
         'Magento_Theme/js/accordion-footer',
     ],
     "map": {
