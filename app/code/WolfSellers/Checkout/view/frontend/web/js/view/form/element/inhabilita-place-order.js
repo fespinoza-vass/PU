a81a55d9ec3afd2deabac4db702d58c9f1d47349
angelica ignacio <angelicaignacio@wolfsellers.com>
2022-11-24 20:44:17 -0600

    promociones

--- app/code/WolfSellers/Checkout/view/frontend/web/js/view/form/element/inhabilita-place-order.js
+++ app/code/WolfSellers/Checkout/view/frontend/web/js/view/form/element/inhabilita-place-order.js
@@ -4,6 +4,12 @@ define([
 ], function($) {
     'use strict';
 
+    $(function() {
+        //$(".action.primary.checkout.amasty").prop("disabled","disabled");
+
+        console.log($('button'));
+    });
+
     $(function(){
         $(document).on('change',"input[name*='agreement']",function (event){
             var selection=event.currentTarget;
@@ -15,4 +21,67 @@ define([
         })
     });
 
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    $.widget('wolfsellers.validacionEnvioEspecial',{
+
+        options:{
+            placeOrder:".payment-method .action.primary.checkout.amasty"
+        },
+        _create(){
+
+            var $placeOrder=$(this.options.placeOrder);
+
+            console.log($placeOrder);
+
+            console.log($("input[name='agreement[1]']"));
+
+
+            
+
+
+
+
+
+            console.log("**************************");
+
+            
+            /*
+            var $inputRadio=$(this.options.radioFacturar);
+            if(this.options.status == 1){
+
+                $inputRadio.each(function(indice, elemento) {
+                    $(elemento).removeProp('checked');
+                })
+                $inputRadio.prop("disabled","disabled");
+            }
+            else{
+                $inputRadio.removeProp("disabled");
+            }
+            
+        }
+    });
+    return $.wolfsellers.validacionEnvioEspecial;
+
+
+        $(document).ready(function(){
+        var $placeOrder=".payment-method .action.primary.checkout";
+
+        console.log($($placeOrder));
+        console.log("##");
+
+
+    });
+    */
 });
