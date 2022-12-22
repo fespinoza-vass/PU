cf47da78e5249c83b55bf90d2341eee29073b98e
Rus0 <andonidominguez@wolfsellers.com>
2022-12-22 13:20:54 -0600

    revert

--- app/design/frontend/Wolfsellers/theme/Amasty_MegaMenuLite/web/js/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    'config': {
-        'mixins': {
-            'Amasty_MegaMenuLite/js/top_menu/item': {
-                'Amasty_MegaMenuLite/js/top_menu/item-mixin': true
-            }
-        }
-    }
-};
