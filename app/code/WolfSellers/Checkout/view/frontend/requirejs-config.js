783f25d927132d748a4422dbb5f67b63b3f15f48
Ing. Diego Velazquez <diegovelazquez@wolfsellers.com>
2023-07-11 15:57:47 -0600

    remove refresh test

--- app/code/WolfSellers/Checkout/view/frontend/requirejs-config.js
+++ app/code/WolfSellers/Checkout/view/frontend/requirejs-config.js
@@ -4,6 +4,9 @@ var config = {
             'Magento_Checkout/js/view/summary/shipping': {
                 'WolfSellers_Checkout/js/view/summary/shipping-mixin': true
             },
+            'Amasty_Label/js/label': {
+                'WolfSellers_Checkout/js/label': true
+            }
         }
     }
 };
