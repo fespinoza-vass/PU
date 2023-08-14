8960ebe42e0e144c9536e8c713bf6265a01c3863
palomino <josepalomino@wolfsellers.com>
2023-08-14 12:03:12 -0600

    show message in the checkout to open modal

--- app/design/frontend/Wolfsellers/theme/Magento_Checkout/web/js/view/payment/benefits.js
+++ app/design/frontend/Wolfsellers/theme/Magento_Checkout/web/js/view/payment/benefits.js
@@ -1,26 +1,16 @@
-require([
+define([
+    'uiComponent',
     'domReady!'
     ],
-    function() {
-        onRenderComplete: function () {
-            var viewModel = this;
-            var popupOptions = {
-                type: 'popup',
-                responsive: true,
-                innerScroll: true,
-                modalClass: 'my-popup',
-                title: '<b>Save Thing</b>',
-                'buttons': [{
-                    text: 'Cancel',
-                    class: 'action'
-                }],
-                opened: function () {
-                    // Because magento modal copies the dom... we need to apply bindings. But it only copies once. So we want to only apply the bindings on first open.
-                    if (this.appliedBindings === undefined) {
-                        ko.applyBindings(viewModel, this);
-                        this.appliedBindings = true;
-                    }
-                }
-            };
+    function(Component) {
+        'use strict';
+        return Component.extend({
+            onRenderComplete: function () {
+                console.log('onRenderComplete');
+            },
+            openBenefits: function () {
+                console.log('openBenefits');
+            }
+        });
     }
 );
