ce79fa0d2e1504284eddac8194a7158df65b8c0b
Aldair Ramirez <aldairramirez@wolfsellers.com>
2023-09-05 17:12:36 -0600

    Se agregan estilos a landing de preguntas frecuentes desktop-mobile,archivo js para anclas

---
+++ app/design/frontend/Wolfsellers/theme/Magento_Theme/web/js/accordion-questions.js
@@ -0,0 +1,36 @@
+require([
+    'jquery',
+    'jquery/ui',
+    'domReady!'
+    ],
+    function($, accordion) {
+       window.onload = function(){
+           setTimeout(() => {
+             $(".row-info-questions > div").accordion({
+               heightStyle: "content",
+               active: true,
+               collapsible: true,
+               autoHeight: false
+             });
+
+            //scroll animation function
+            $('.pagebuilder-button-primary').click(function(e){
+                e.preventDefault();
+                var target = $($(this).attr('href'));
+                if(target.length){
+                  var scrollTo = target.offset().top - 160;
+                  $('body, html').animate({scrollTop: scrollTo+'px'}, 500);
+                  $('.content-menu-questions').removeClass('active');
+                  $('.button-primary-mobile-questions').removeClass('active');
+                }
+            });
+
+            $(".button-primary-mobile-questions").click(function () {
+                $(this).toggleClass("active");
+                $('.content-menu-questions').toggleClass("active");
+            });
+
+           }, 5000);
+       }
+    }
+);
