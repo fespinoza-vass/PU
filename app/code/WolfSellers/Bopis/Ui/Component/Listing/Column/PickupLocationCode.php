efdfcc8f53367e2f4ffe6d021f8dc8103c5eb73d
Cesar Rojas <cesarrojas@wolfsellers.com>
2023-09-11 14:59:55 -0600

    columna tienda solo para pickup

---
+++ app/code/WolfSellers/Bopis/Ui/Component/Listing/Column/PickupLocationCode.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace WolfSellers\Bopis\Ui\Component\Listing\Column;
+
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\InventoryApi\Api\SourceRepositoryInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\View\Element\UiComponentInterface;
+use Magento\Ui\Component\AbstractComponent;
+
+
+class PickupLocationCode extends Column
+{
+    const SHIPPING_INFORMATION = 'shipping_information';
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $sourceCode = $item[$this->getData('name')];
+                $sourceName = '-';
+                if ($sourceCode){
+                    if (isset($item[self::SHIPPING_INFORMATION])) {
+                        $sourceName = explode('-', $item[self::SHIPPING_INFORMATION]);
+                        $sourceName = is_array($sourceName) ? end($sourceName) : '';
+                    }
+                }
+                $item[$this->getData('name')] = $sourceName ?? $sourceCode;
+            }
+        }
+
+        return $dataSource;
+    }
+}
