c189afda81b0e346444bfecb56bdfb952390a70a
Cesar Rojas <cesarrojas@wolfsellers.com>
2023-10-06 10:23:03 -0600

    emails bopis

--- app/code/WolfSellers/Email/Helper/EmailHelper.php
+++ app/code/WolfSellers/Email/Helper/EmailHelper.php
@@ -6,7 +6,12 @@ namespace WolfSellers\Email\Helper;
 
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order as ModelOrder;
+use Magento\Sales\Model\OrderFactory;
 use WolfSellers\Email\Model\Email\Identity\SatisfactionSurvey;
+use WolfSellers\Email\Model\Email\Identity\PreparedOrder;
+use WolfSellers\Email\Model\Email\Identity\ShipOrder;
+use WolfSellers\Email\Model\Email\Identity\ReadyToPickupOrder;
 use WolfSellers\Email\Model\Email\SimpleSender;
 
 class EmailHelper
@@ -14,11 +19,20 @@ class EmailHelper
     /**
      * @param SimpleSender $simpleSender
      * @param SatisfactionSurvey $satisfactionSurvey
+     * @param PreparedOrder $preparedOrder
+     * @param ShipOrder $shipOrder
+     * @param ReadyToPickupOrder $readyToPickupOrder
+     * @param OrderFactory $orderFactory
      */
     public function __construct(
-        protected SimpleSender $simpleSender,
-        protected SatisfactionSurvey $satisfactionSurvey
-    ) {
+        protected SimpleSender       $simpleSender,
+        protected SatisfactionSurvey $satisfactionSurvey,
+        protected PreparedOrder      $preparedOrder,
+        protected ShipOrder          $shipOrder,
+        protected ReadyToPickupOrder $readyToPickupOrder,
+        protected OrderFactory       $orderFactory
+    )
+    {
     }
 
     /**
@@ -27,7 +41,7 @@ class EmailHelper
      * @return bool
      * @throws NoSuchEntityException
      */
-    public function sendSatisfactionSurveyEmail($to, Order $order) : bool
+    public function sendSatisfactionSurveyEmail($to, Order $order): bool
     {
         $surveyEmail = $this->satisfactionSurvey;
 
@@ -35,7 +49,91 @@ class EmailHelper
             return false;
         }
 
-        $vars = [
+        $vars = $this->getGeneralVars($order);
+
+        $sender = $this->simpleSender;
+        $sender->setTemplateIdentifier($surveyEmail->getTemplateId());
+        $sender->setSender($surveyEmail->getEmailIdentity());
+        $sender->send($to, $vars);
+        return true;
+    }
+
+    /**
+     * @param $to
+     * @param Order $order
+     * @return bool
+     * @throws NoSuchEntityException
+     */
+    public function sendPreparedOrderEmail($to, Order $order): bool
+    {
+        $preparedOrder = $this->preparedOrder;
+
+        if (!$preparedOrder->isEnabled()) {
+            return false;
+        }
+
+        $vars = $this->getGeneralVars($order);
+
+        $sender = $this->simpleSender;
+        $sender->setTemplateIdentifier($preparedOrder->getTemplateId());
+        $sender->setSender($preparedOrder->getEmailIdentity());
+        $sender->send($to, $vars);
+        return true;
+    }
+
+    /**
+     * @param $to
+     * @param ModelOrder $order
+     * @return bool
+     * @throws NoSuchEntityException
+     */
+    public function sendShipOrderEmail($to, Order $order): bool
+    {
+        $shipOrder = $this->shipOrder;
+
+        if (!$shipOrder->isEnabled()) {
+            return false;
+        }
+
+        $vars = $this->getGeneralVars($order);
+
+        $sender = $this->simpleSender;
+        $sender->setTemplateIdentifier($shipOrder->getTemplateId());
+        $sender->setSender($shipOrder->getEmailIdentity());
+        $sender->send($to, $vars);
+        return true;
+    }
+
+    /**
+     * @param $to
+     * @param ModelOrder $order
+     * @return bool
+     * @throws NoSuchEntityException
+     */
+    public function sendReadyToPickupOrderEmail($to, Order $order): bool
+    {
+        $readyToPickupOrder = $this->readyToPickupOrder;
+
+        if (!$readyToPickupOrder->isEnabled()) {
+            return false;
+        }
+
+        $vars = $this->getGeneralVars($order);
+
+        $sender = $this->simpleSender;
+        $sender->setTemplateIdentifier($readyToPickupOrder->getTemplateId());
+        $sender->setSender($readyToPickupOrder->getEmailIdentity());
+        $sender->send($to, $vars);
+        return true;
+    }
+
+    /**
+     * @param ModelOrder $order
+     * @return array
+     */
+    public function getGeneralVars(Order $order): array
+    {
+        return [
             'order' => $order,
             'order_id' => $order->getId(),
             'billing' => $order->getBillingAddress(),
@@ -48,15 +146,17 @@ class EmailHelper
                 'email_customer_note' => $order->getEmailCustomerNote(),
                 'frontend_status_label' => $order->getFrontendStatusLabel()
             ],
-            'created_at_formatted' => $order->getCreatedAtFormatted(2),
-            'satisfaction_survey_url' => $surveyEmail->getSatisfactionSurveyUrl()
+            'created_at_formatted' => $order->getCreatedAtFormatted(2)
         ];
+    }
 
-        $sender = $this->simpleSender;
-        $sender->setTemplateIdentifier($surveyEmail->getTemplateId());
-        $sender->setSender($surveyEmail->getEmailIdentity());
-        $sender->send($to, $vars);
-        return true;
+    /**
+     * @param $order
+     * @return ModelOrder
+     */
+    public function getOrderModel($order)
+    {
+        return $this->orderFactory->create()->loadByIncrementId($order->getIncrementId());
     }
 }
 
