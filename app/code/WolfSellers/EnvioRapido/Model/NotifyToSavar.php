aa3b1a9186b485a778f359513bfeac0ecfc7e71e
Brandon Juarez Ponce <brandonjuarez@wolfsellers.com>
2023-10-06 14:34:11 -0600

    PULMDEV-87 Adding comments to README

---
+++ app/code/WolfSellers/EnvioRapido/Model/NotifyToSavar.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WolfSellers\EnvioRapido\Model;
+
+use Psr\Log\LoggerInterface;
+
+use WolfSellers\EnvioRapido\Model\SavarApi;
+
+class NotifyToSavar extends SavarApi
+{
+
+    protected function getRequest($data)
+    {
+        // TODO: Implement getRequest() method.
+    }
+
+    protected function getUri()
+    {
+        // TODO: Implement getUri() method.
+    }
+
+    protected function getMethodType()
+    {
+        // TODO: Implement getMethodType() method.
+    }
+}
