c4eed404405c76729cba7700e5c15b364a6d0be8
Aldair Ramirez <aldairramirez@wolfsellers.com>
2023-10-09 13:36:42 -0600

    estilos

--- app/design/frontend/Wolfsellers/theme/Magento_Theme/web/js/home.js
+++ app/design/frontend/Wolfsellers/theme/Magento_Theme/web/js/home.js
@@ -156,6 +156,23 @@ require([
 	        });
     	}
 
+		//detects scroll and hides buttons if class exists
+		$(window).scroll(function(){
+			if($(window).scrollTop() > 100){
+				if($('.nav-header-links.sticky-header-buttons').length === 0){
+					$('.nav-header-links').css('display','none');
+				}else{
+					$('.nav-header-links').css('display','block');
+				}
+			}else{
+				$('.nav-header-links').css('display','block');
+			}
+		});
+
+		if($('.nav-header-links').length === 1){
+			$('body').addClass('active-buttons-header');
+		}
+
     });
 
 });
