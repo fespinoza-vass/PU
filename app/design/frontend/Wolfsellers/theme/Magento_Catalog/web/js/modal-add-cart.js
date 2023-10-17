fbb079af336a46bfedf9a407dce55c45a42ba900
Alexis hernandez <alexishernandez@wolfsellers.com>
2023-10-17 16:21:57 -0600

    Creación Pop-Ups de Cambio de Envío en PDP y Mini carrito

---
+++ app/design/frontend/Wolfsellers/theme/Magento_Catalog/web/js/modal-add-cart.js
@@ -0,0 +1,54 @@
+require([
+    'jquery',
+    'Magento_Ui/js/modal/modal',
+    'domReady!'
+],function(
+    $,
+    modal,
+) {
+    'use strict';
+    var options = {
+            modalSelector: '#cart-modal-products',
+        },
+        productModal;
+
+
+    $(document).on('click', '.tocart' , function (event) {
+        let idButtonAddProduct = "#" + this.id;
+        event.preventDefault();
+
+        //validacion logica de entrega rapida ::
+        //if(n == true) {
+//popup modal code
+        var popup = $('<div class="add-to-cart-modal-popup"/>').html($('.page-title span').text() + '<span> has been added to cart.</span>').modal({
+            modalClass: 'add-to-cart-popup',
+            title: $.mage.__("Popup Title"),
+            buttons: [
+                {
+                    text: $.mage.__("Editar Carrito"),
+                    click: function () {
+                        window.location = window.checkout.shoppingCartUrl
+                        //this.closeModal();
+                    }
+                },
+                {
+                    text: $.mage.__("Continuar compra"),
+                    click: function () {
+                        window.location = window.checkout.checkoutUrl
+                    }
+                }
+            ]
+        });
+        popup.modal('openModal');
+
+        //}
+        event.preventDefault();
+    });
+
+});
+
+
+
+
+
+
