be2c67e09c273d303e267c45633c24eff81568ad
Victor Ruiz <victor.ruiz@wolfsellers.com>
2023-10-25 19:45:21 -0600

    se crean columnas de los atributos de la direccion de facturacion  y se manda la informacion para guardar

--- app/code/WolfSellers/Checkout/view/frontend/web/js/view/invoice-checkout-form.js
+++ app/code/WolfSellers/Checkout/view/frontend/web/js/view/invoice-checkout-form.js
@@ -33,6 +33,10 @@ define([
          * If invoice require, change billing address data
          */
         onSubmit: function() {
+            if(quote.billingAddress().extensionAttributes !== undefined && quote.billingAddress().extensionAttributes.pickup_location_code !== undefined){
+                var pickup_location_code = quote.billingAddress().extensionAttributes.pickup_location_code;
+            }
+            quote.billingAddress().extensionAttributes = {};
             // trigger form validation
             this.source.set('params.invalid', false);
             this.source.trigger('customCheckoutForm.data.validate');
@@ -64,16 +68,20 @@ define([
                 }
                 customAtributes.forEach( function(value, index, array) {
                     if(value['attribute_code'] === 'invoice_required'){
+                        quote.billingAddress().extensionAttributes.invoice_required = 1;
                         value['value'] = 1;
                         value['label'] = 'Si';
                     }
                     if(value['attribute_code'] === 'razon_social'){
+                        quote.billingAddress().extensionAttributes.razon_social = formData.razon_social;
                         value['value'] = formData.razon_social;
                     }
                     if(value['attribute_code'] === 'direccion_fiscal'){
+                        quote.billingAddress().extensionAttributes.direccion_fiscal = formData.direccion_fiscal;
                         value['value'] = formData.direccion_fiscal;
                     }
                     if(value['attribute_code'] === 'ruc'){
+                        quote.billingAddress().extensionAttributes.ruc = formData.ruc;
                         value['value'] = formData.ruc;
                     }
                 });
@@ -83,8 +91,14 @@ define([
                 $('input[name="direccion_fiscal"]').prop('disabled', 'disabled');
                 $('#submitInvoice').hide();
                 $('#editInvoice').show();
-                quote.billingAddress().extensionAttributes['customAttributes'] = customAtributes;
-                quote.billingAddress().customAttributes = customAtributes;
+
+                if(quote.billingAddress().extensionAttributes !== undefined && quote.billingAddress().extensionAttributes.pickup_location_code !== undefined){
+                    quote.billingAddress().extensionAttributes.pickup_location_code = pickup_location_code;
+                }
+
+
+
+                //quote.billingAddress().customAttributes = customAtributes;
                 console.log(quote.shippingAddress());
                 messageList.addSuccessMessage({ message: $.mage.__('Información de facturación gurdada') });
 
