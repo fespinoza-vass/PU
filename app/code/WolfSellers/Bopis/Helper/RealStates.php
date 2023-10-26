a8b0c05875de6c8d96219e2de3682ac27961db0c
Ing. Diego Velazquez <diegovelazquez@wolfsellers.com>
2023-10-25 18:50:21 -0600

    remove realstate pending state

--- app/code/WolfSellers/Bopis/Helper/RealStates.php
+++ app/code/WolfSellers/Bopis/Helper/RealStates.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace WolfSellers\Bopis\Helper;
+use WolfSellers\Bopis\Model\ResourceModel\AbstractBopisCollection;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 
 class RealStates
 {
+    const ENVIO_RAPIDO = 'envio_rapido';
     /** @var string */
     const STATE_PATH = 'bopis/status/';
 
@@ -39,10 +41,16 @@ class RealStates
      */
     public function getShippingMethodTitle($shippingMethodCode): string
     {
-        if (!$shippingMethodCode) return '';
-        $code = explode('_', $shippingMethodCode);
-       // return $this->_scopeConfig->getValue('carriers/' . $code[0] . '/title');
-        return "";
+//        if (!$shippingMethodCode) return '';
+//
+//        if ($shippingMethodCode == AbstractBopisCollection::FAST_SHIPPING_METHOD){
+//            $code = self::ENVIO_RAPIDO;
+//        }else{
+//            $code = explode('_', $shippingMethodCode);
+//        }
+//
+//        return $this->_scopeConfig->getValue('carriers/' . $code[0] . '/title');
+        return '';
     }
 
     /**
@@ -61,12 +69,6 @@ class RealStates
     private function initializeStates(): void
     {
         $this->states = [
-            'pending' => [
-                'state' => '',
-                'label' => 'Pendiente',
-                'action' => '#',
-                'menu' => 'listnewsorders'
-            ],
             'confirmed' => [
                 'state' => '',
                 'label' => 'Pedido Confirmado',
