cc3bf79a4579965c4da9b011b14f5fc932cf8bec
Ing. Diego Velazquez <diegovelazquez@wolfsellers.com>
2023-11-09 22:38:20 -0600

    hotfix try catch

--- app/code/WolfSellers/Sources/Plugin/Ui/DataProvider/SourceDataProviderPlugin.php
+++ app/code/WolfSellers/Sources/Plugin/Ui/DataProvider/SourceDataProviderPlugin.php
@@ -5,16 +5,20 @@ namespace WolfSellers\Sources\Plugin\Ui\DataProvider;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\InventoryAdminUi\Ui\DataProvider\SourceDataProvider;
 use Magento\InventoryApi\Api\SourceRepositoryInterface;
+use Psr\Log\LoggerInterface;
 
 class SourceDataProviderPlugin
 {
+    protected $_logger;
     /**
      * @param SourceRepositoryInterface $_sourceRepository
      */
     public function __construct(
-        protected SourceRepositoryInterface $_sourceRepository
+        protected SourceRepositoryInterface $_sourceRepository,
+        LoggerInterface $logger
     )
     {
+        $this->_logger = $logger;
     }
 
     /**
@@ -25,28 +29,22 @@ class SourceDataProviderPlugin
      */
     public function afterGetData(SourceDataProvider $subject, array $result): array
     {
-        if ('inventory_source_form_data_source' === $subject->getName()) {
-            if (isset($result['items'])) {
-                foreach ($result['items'] as &$item) {
-                    $sourceCode = $item['source_code'] ?? null;
-                    if ($sourceCode) {
-                        try {
-                            $sourceItem = $this->_sourceRepository->get($sourceCode);
-                            $item['bopis']['available_shipping_methods'] = $sourceItem->getData('available_shipping_methods');
-                            $item['bopis']['extension_attributes']['is_fastshipping_active'] = $sourceItem->getData('is_fastshipping_active');
-                            $item['bopis']['extension_attributes']['conductor'] = $sourceItem->getData('conductor');
-                            $item['bopis']['extension_attributes']['range_radius'] = $sourceItem->getData('range_radius');
-                            $item['bopis']['is_fastshipping_active'] = $sourceItem->getData('is_fastshipping_active');
-                            $item['bopis']['conductor'] = $sourceItem->getData('conductor');
-                            $item['bopis']['range_radius'] = $sourceItem->getData('range_radius');
-                            $item['general']['district'] = $sourceItem->getData('district');
-                        } catch (NoSuchEntityException $e) {
-                            // @todo
-                        }
-                    }
+        try{
+            if ('inventory_source_form_data_source' === $subject->getName()) {
+                foreach ($result as $key => $value) {
+                    $item = $this->_sourceRepository->get($key);
+                    $result[$key]['bopis']['available_shipping_methods'] = $item->getData('available_shipping_methods');
+                    $result[$key]['bopis']['extension_attributes']['is_fastshipping_active'] = $item->getData('is_fastshipping_active');
+                    $result[$key]['bopis']['extension_attributes']['conductor'] = $item->getData('conductor');
+                    $result[$key]['bopis']['extension_attributes']['range_radius'] = $item->getData('range_radius');
+                    $result[$key]['bopis']['is_fastshipping_active'] = $item->getData('is_fastshipping_active');
+                    $result[$key]['bopis']['conductor'] = $item->getData('conductor');
+                    $result[$key]['bopis']['range_radius'] = $item->getData('range_radius');
+                    $result[$key]['general']['district'] = $item->getData('district');
                 }
-                unset($item);
             }
+        }catch (\Exception $error){
+            $this->_logger->error($error->getMessage());
         }
 
         return $result;
