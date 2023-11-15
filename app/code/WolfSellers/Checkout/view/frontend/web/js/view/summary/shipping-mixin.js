ea73d2ce2b1ada6a3abc52dd9a20f60773919d2a
Rus0 <andonidominguez@wolfsellers.com>
2023-11-15 15:23:41 -0600

    hail mary v2

--- app/code/WolfSellers/Checkout/view/frontend/web/js/view/summary/shipping-mixin.js
+++ app/code/WolfSellers/Checkout/view/frontend/web/js/view/summary/shipping-mixin.js
@@ -15,51 +15,20 @@ define([
     var mixin = {
         customTitle: ko.observable(''),
         initialize : function () {
-          var self = this;
-          this._super();
-          quote.shippingMethod.subscribe(function (value) {
+            var self = this;
+            this._super();
+            quote.shippingMethod.subscribe(function (value) {
                 if(!_.isUndefined(value) && !_.isNull(value) && !value.carrier_code.includes('instore')){
                     if (value && value.method_title) {
-                        var payload = {
-                            'ubigeo': quote.shippingAddress().postcode
-                        };
-                        $.ajax({
-                            url: url.build('zipcode/index/getubigeo'),
-                            dataType: 'json',
-                            data: payload,
-                            global: false
-                        }).done(function (estimated) {
-                            if (estimated) {
-                                estimated = JSON.parse(estimated);
-                                self.customTitle(value['method_title'] + ' ' + estimated.data );
-                            } else {
-                                self.customTitle(value['method_title']);
-                            }
-                        })
+                        self.customTitle(value['method_title']);
                     }
                 }
-          });
-        quote.shippingAddress.subscribe(function (value) {
-            var _shippingMethod = quote.shippingMethod();
+            });
+            quote.shippingAddress.subscribe(function (value) {
+                var _shippingMethod = quote.shippingMethod();
 
-            if (_shippingMethod && _shippingMethod.method_title) {
-                var payload = {
-                    'ubigeo': quote.shippingAddress().postcode
-                };
-                $.ajax({
-                    type: 'POST',
-                    url: url.build('zipcode/index/getubigeo'),
-                    dataType: 'json',
-                    data: payload,
-                    global: false
-                }).done(function (estimated) {
-                    if (estimated) {
-                        estimated = JSON.parse(estimated);
-                        self.customTitle(_shippingMethod['method_title'] + ' ' + estimated.data );
-                    } else {
-                        self.customTitle(_shippingMethod['method_title']);
-                    }
-                })
+                if (_shippingMethod && _shippingMethod.method_title) {
+                    self.customTitle(_shippingMethod['method_title']);
                 }
             });
         },
