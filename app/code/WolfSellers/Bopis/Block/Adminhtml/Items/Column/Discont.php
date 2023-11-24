aff1b0306470a4c5579c2b1c5f2fb5ad9ea13721
Cesar Rojas <cesarrojas@wolfsellers.com>
2023-11-24 16:24:16 -0600

    update comment

--- app/code/WolfSellers/Bopis/Block/Adminhtml/Items/Column/Discont.php
+++ app/code/WolfSellers/Bopis/Block/Adminhtml/Items/Column/Discont.php
@@ -1,16 +1,9 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
+
 namespace WolfSellers\Bopis\Block\Adminhtml\Items\Column;
 
 /**
- * Sales Order items qty column renderer
- *
- * @api
- * @author     Magento Core Team <core@magentocommerce.com>
- * @since 100.0.2
+ * Sales Order items discount column renderer
  */
 class Discont extends \Magento\Sales\Block\Adminhtml\Items\Column\DefaultColumn
 {
