47dfe7acd04ffbc3461a2ce09763be7e6d7a5fba
Rene <renecastillo@wolfsellers.com>
2023-11-27 15:57:31 -0600

    feature/stock-distance-algorithm

---
+++ app/code/WolfSellers/InStorePickup/Model/Config/Source/Strategy.php
@@ -0,0 +1,18 @@
+<?php
+namespace WolfSellers\InStorePickup\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+
+/**
+ * @class Strategy
+ */
+class Strategy implements OptionSourceInterface
+{
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 'distance_based', 'label' => __('Distance Based')],
+            ['value' => 'stock_distance_based', 'label' => __('Stock Distance Based')]
+        ];
+    }
+}
