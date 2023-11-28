afd0b1e5010cb41350e61d4c9c6c9f8c31abe647
Alexis Tapia <alexistapia@wolfsellers.com>
2023-11-28 17:39:08 -0600

    Remove ui component

--- app/design/frontend/Wolfsellers/theme/Amasty_Xsearch/web/js/components/survey.js
+++ app/design/frontend/Wolfsellers/theme/Amasty_Xsearch/web/js/components/survey.js
@@ -1,23 +1,13 @@
 define([
-    'uiComponent',
     'jquery',
-    'underscore'
-], function (Component, $, _) {
-    return Component.extend({
-
-        /**
-         * Get url param id value, id reference increment id order to survey
-         * @returns {*}
-         */
-        initialize: function () {
-            let params = new URL(document.location).searchParams;
-            if(!_.isUndefined(params)){
-                let increment_id = params.get("id");
-                if(!_.isUndefined(increment_id)){
-                    $('input.num-order').val(increment_id);
-                }
-            }
-            return this._super();
-        },
-    });
+    'underscore',
+    'domReady!'
+], function ($, _) {
+    var params =  new URL(document.location).searchParams;
+    if(!_.isUndefined(params)){
+        var increment_id = params.get("id");
+        if(!_.isUndefined(increment_id)){
+            $('input.num-order').val(increment_id);
+        }
+    }
 });
