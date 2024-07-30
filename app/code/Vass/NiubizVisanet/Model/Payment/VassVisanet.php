d25c5c922a406b11a816de0b205eadbd5118de07
Victor Sandoval <sandovalceav@gmail.com>
2024-07-30 12:44:15 -0400

    Changes Vass Niubiz Visanet -add library to validate payment -add csp whitelist -create controller to generate token session

--- app/code/Vass/NiubizVisanet/Model/Payment/VassVisanet.php
+++ app/code/Vass/NiubizVisanet/Model/Payment/VassVisanet.php
@@ -15,6 +15,73 @@ class VassVisanet extends \Magento\Payment\Model\Method\Cc
 
     protected $_canAuthorize = true;
     protected $_canCapture = true;
+    protected $logger;
+    protected $configNiubiz;
+    protected $encryptor;
+
+    /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
+     * @param \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory
+     * @param \Magento\Payment\Helper\Data $paymentData
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Payment\Model\Method\Logger $logger
+     * @param \Magento\Framework\Module\ModuleListInterface $moduleList
+     * @param \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate
+     * @param ProFactory $proFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     * @param \Magento\Framework\App\RequestInterface $requestHttp
+     * @param CartFactory $cartFactory
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+     * @param array $data
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Payment\Model\Method\Logger $logger,
+        \Magento\Framework\Module\ModuleListInterface $moduleList,
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
+        \Vass\NiubizVisanet\Model\Config $configNiubiz,
+        \Magento\Framework\Encryption\EncryptorInterface $encryptor,
+        \Magento\Paypal\Model\ProFactory $proFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\UrlInterface $urlBuilder,
+        \Magento\Framework\App\RequestInterface $requestHttp,
+        \Magento\Paypal\Model\CartFactory $cartFactory,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $scopeConfig,
+            $logger,
+            $moduleList,
+            $localeDate,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+        $this->_storeManager = $storeManager;
+        $this->_urlBuilder = $urlBuilder;
+        $this->_requestHttp = $requestHttp;
+        $this->_cartFactory = $cartFactory;
+        $this->_logger = $logger;
+        $this->_configNiubiz = $configNiubiz;
+        $this->encryptor = $encryptor;
+    }
 
     /**
      * Capture Payment.
@@ -26,6 +93,7 @@ class VassVisanet extends \Magento\Payment\Model\Method\Cc
     public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
     {
         try {
+
             //check if payment has been authorized
             if(is_null($payment->getParentTransactionId())) {
                 $this->authorize($payment, $amount);
@@ -72,6 +140,7 @@ class VassVisanet extends \Magento\Payment\Model\Method\Cc
                 'amount' => $amount
             ];
 
+            // $this->_logger->debug('hh');
             //check if payment has been authorized
             $response = $this->makeAuthRequest($request);
 
--- app/code/Vass/NiubizVisanet/view/frontend/web/js/view/payment/method-renderer/vassvisanet-method.js
+++ app/code/Vass/NiubizVisanet/view/frontend/web/js/view/payment/method-renderer/vassvisanet-method.js
@@ -1,13 +1,23 @@
 define([
     'jquery',
-    'Magento_Payment/js/view/payment/cc-form'
+    'Magento_Payment/js/view/payment/cc-form',
+    'mage/url',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Vault/js/view/payment/vault-enabler',
+    'Magento_Checkout/js/model/full-screen-loader',
+    'Magento_Customer/js/customer-data',
+    'payform'
 ],
-function ($, Component) {
+function ($, Component, url, quote, VaultEnabler,fullScreenLoader,customerData,paylib
+    ) {
     'use strict';
 
     return Component.extend({
         defaults: {
-            template: 'Vass_NiubizVisanet/payment/vassvisanet'
+            template: 'Vass_NiubizVisanet/payment/vassvisanet',
+            confPaymentUrl: url.build('vassvisanet/visa/hello'),
+            additionalData: {},
+            paymenToken: null
         },
 
         context: function() {
@@ -20,7 +30,132 @@ function ($, Component) {
 
         isActive: function() {
             return true;
+        },
+
+        getData: function () {
+
+            var data = {
+
+                'method': this.getCode(),
+
+                'additional_data': {
+
+                    'payment_token': this.paymenToken
+
+                }
+
+            };
+
+
+            return data;
+
+        },
+
+        /**
+         * Place order
+         */
+         beforePlaceOrder: function () {
+            this.getPaymentConf();
+        },
+
+        /**
+             * Get full selector name
+             *
+             * @param {String} field
+             * @returns {String}
+             */
+         getSelector: function (field) {
+            return '#' + this.getCode() + '_' + field;
+        },
+
+        getCustomerDetails: function () {
+            var billingAddress = quote.billingAddress();
+            return {
+                firstName: billingAddress.firstname,
+                lastName: billingAddress.lastname,
+                phone: billingAddress.telephone,
+                email: typeof quote.guestEmail === 'string' ? quote.guestEmail : window.checkoutConfig.customerData.email
+            }
+        },
+
+        sendPaymentVal: function(sessionToken,merchantId,totals) {
+
+            var data = {
+                name: this.getCustomerDetails().firstName,
+                lastName: this.getCustomerDetails().lastname,
+                email: this.getCustomerDetails().email
+                };
+
+            var configuration = {
+                sessionkey: sessionToken,
+                channel: 'web',
+                merchantid: merchantId,
+                purchasenumber: quote.getQuoteId(),
+                amount: totals,
+                language: 'es',
+                font: 'https://fonts.googleapis.com/css?family=Montserrat:400&display=swap',
+                };
+                window.payform.setConfiguration(configuration);
+                var cardNumber = this.getSelector('cc_number');
+                var cardExpiry = this.getSelector('expirationDate');
+                var cardCvc = this.getSelector('cc_cid');
+
+                console.log(data);
+                console.log(configuration);
+
+                return cardNumber+cardExpiry+cardCvc;
+                /* Caso de uso: Controles independientes */
+                // window.payform.createToken([cardNumber,cardExpiry,cardCvc], data).then(function(response){
+                // return response;
+                //     }).catch(function(error){
+                //         fullScreenLoader.stopLoader();
+                //         globalMessageList.addErrorMessage({
+                //             message: error.message
+                //         });
+                // })
+        },
+
+        /**
+         * Send request to get payment method nonce
+         */
+         getPaymentConf: function () {
+            var self = this;
+            alert('aqui');
+
+            fullScreenLoader.startLoader();
+            $.getJSON(self.confPaymentUrl, {
+                data: JSON.stringify({})
+            })
+                .done(function (response) {
+                    fullScreenLoader.stopLoader();
+                    let total = quote.getCalculatedTotal();
+
+                    var payment = self.sendPaymentVal(response.token, response.merchant, total);
+                    console.log(payment);
+
+                    fullScreenLoader.stopLoader();
+
+                    this.paymenToken = payment;
+                    if(this.paymenToken !== null){
+                        self.placeOrder();
+                    }else{
+                        fullScreenLoader.stopLoader();
+                        globalMessageList.addErrorMessage({
+                            message: 'Intente nuevamente'
+                        });
+                    }
+                })
+                .fail(function (response) {
+                    var error = JSON.parse(response.responseText);
+
+                    fullScreenLoader.stopLoader();
+                    globalMessageList.addErrorMessage({
+                        message: error.message
+                    });
+                });
         }
     });
 }
 );
+
+
