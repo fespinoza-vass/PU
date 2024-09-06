45ed2943489cc308e7f5b6ea4be17d35ae9237f7
Victor Sandoval Cea <victor.sandoval@vasscompany.com>
2024-09-06 09:46:45 -0400

    Urbano files to desgin

--- app/code/WolfSellers/Urbano/Model/Carrier/Urbano.php
+++ app/code/WolfSellers/Urbano/Model/Carrier/Urbano.php
@@ -303,21 +303,6 @@ class Urbano extends AbstractCarrierOnline implements CarrierInterface
         $insuranceCost = $insurance['valor_ennvio'] ?? 0;
         $igv = (float) $this->getConfigData('igv');
 
-        // Split methods.
-        if (!empty($quoteService['valor_ennvio']) && (float) $quoteService['valor_ennvio'] > 0) {
-            $cost = (float) $quoteService['valor_ennvio'];
-
-            if ($igv > 0) {
-                $cost += $cost * ($igv / 100);
-            }
-
-            $quoteMethods[] = [
-                'method' => self::METHOD_TERRESTRE,
-                'cost' => $cost + $insuranceCost,
-                'time' => $quoteService['time_envio'],
-            ];
-        }
-
         if (!empty($quoteService['valor_envio_aereo']) && (float) $quoteService['valor_envio_aereo'] > 0) {
             $cost = (float) $quoteService['valor_envio_aereo'];
 
