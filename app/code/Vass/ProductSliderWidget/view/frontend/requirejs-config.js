374d03907c591c7150e810bf515262cea9705662
Carlos Javier Rodriguez Martin <carlos.rodriguez@vasscompany.com>
2024-09-12 08:47:39 +0200

    Create widget products slider

---
+++ app/code/Vass/ProductSliderWidget/view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * @copyright Copyright (c) 2024 VASS
+ * @package Vass_ProductSliderWidget
+ * @author VASS Team
+ */
+
+var config = {
+    map: {
+        '*': {
+            productsSlider: 'Vass_ProductSliderWidget/js/products-slider'
+        }
+    }
+};
