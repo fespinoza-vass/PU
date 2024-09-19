2816e08a2a4f255839e4359664992485a87bb48c
Carlos Javier Rodriguez Martin <carlos.rodriguez@vasscompany.com>
2024-09-19 12:03:22 +0200

    Create module promotions cards

---
+++ app/code/Vass/PromotionsSliderCards/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright Copyright (c) 2024 VASS
+ * @package Vass_PromotionsSliderCards
+ * @author VASS Team
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Vass_PromotionsSliderCards', __DIR__);
---
+++ app/code/Vass/PromotionsSliderCards/view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * @copyright Copyright (c) 2024 VASS
+ * @package Vass_PromotionsSliderCards
+ * @author VASS Team
+ */
+
+var config = {
+    map: {
+        '*': {
+            promotionsCardsSlider: 'Vass_PromotionsSliderCards/js/slider'
+        }
+    }
+};
