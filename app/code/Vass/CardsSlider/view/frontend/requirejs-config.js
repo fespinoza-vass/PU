2faf0227962da54d3156c886c67a80cec8dbe6d3
joseantonio.marquez <joseantonio.marquez@vasscompany.com>
2024-09-19 17:50:50 +0200

    Add cardsSlider module to config map

---
+++ app/code/Vass/CardsSlider/view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * @copyright Copyright (c) 2024 VASS
+ * @package Vass_CardsSlider
+ * @author VASS Team
+ */
+
+var config = {
+    map: {
+        '*': {
+            cardsSlider: 'Vass_CardsSlider/js/cards-slider'
+        }
+    }
+};
