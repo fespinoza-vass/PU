bf9cae26566424ecfcfceee836e9188290cf8d19
joseantonio.marquez <joseantonio.marquez@vasscompany.com>
2024-09-26 18:53:31 +0200

    Add copyrights

--- app/code/Vass/CardsSlider/registration.php
+++ app/code/Vass/CardsSlider/registration.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @copyright Copyright (c) 2024 VASS
+ * @package Vass_CardsSlider
+ * @author VASS Team
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
