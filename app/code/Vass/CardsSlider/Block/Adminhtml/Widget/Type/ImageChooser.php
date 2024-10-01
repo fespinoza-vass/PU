ae81ba720df8532c50bcc786c2f864d1db0c13b8
joseantonio.marquez <joseantonio.marquez@vasscompany.com>
2024-10-01 10:22:58 +0200

    Fix: refactoring

--- app/code/Vass/CardsSlider/Block/Adminhtml/Widget/Type/ImageChooser.php
+++ app/code/Vass/CardsSlider/Block/Adminhtml/Widget/Type/ImageChooser.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @copyright Copyright (c) 2024 VASS
+ * @package Vass_CardsSlider
+ * @author VASS Team
+ */
+
 namespace Vass\CardsSlider\Block\Adminhtml\Widget\Type;
 
 use Magento\Backend\Block\Template;
@@ -9,13 +15,15 @@ use Magento\Framework\Data\Form\Element\Factory;
 use Magento\Framework\Data\Form\Element\Factory as FormElementFactory;
 use Magento\Framework\Data\Form\Element\Text;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\SomeModule\ElementFactory;
+use Magento\Ui\DataProvider\Mapper\FormElement;
 
 class ImageChooser extends Template
 {
     /**
      * @var Factory
      */
-    protected $_elementFactory;
+    protected FormElementFactory $_elementFactory;
     /**
      * @param TemplateContext $context
      * @param FormElementFactory $elementFactory
