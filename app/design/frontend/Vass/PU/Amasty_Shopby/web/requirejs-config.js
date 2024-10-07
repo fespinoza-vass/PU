cce4d0b59c97a30f77a6acd63020ddc1e9d5a6be
joseantonio.marquez <joseantonio.marquez@vasscompany.com>
2024-10-07 17:48:07 +0200

    Add Js to requirejs-config.js

---
+++ app/design/frontend/Vass/PU/Amasty_Shopby/web/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    map: {
+        '*': {
+            amShopbyFilterAbstract: 'Amasty_Shopby/js/amShopby',
+            amShopbyApplyFilters: 'Amasty_Shopby/js/amShopbyApplyFilters',
+            amShopbyTopFilters: 'Amasty_Shopby/js/amShopbyTopFilters',
+        }
+    },
+};
