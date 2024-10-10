7178ed919995cdfaa70abfd336035246a8c0ad43
joseantonio.marquez <joseantonio.marquez@vasscompany.com>
2024-10-10 12:10:25 +0200

    Refactor location Amasty_Shopby to Amasty_ShopbyBase

--- app/design/frontend/Vass/PU/Amasty_Shopby/web/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            amShopbyFilterAbstract: 'Amasty_Shopby/js/amShopby',
-            amShopbyApplyFilters: 'Amasty_Shopby/js/amShopbyApplyFilters',
-            amShopbyTopFilters: 'Amasty_Shopby/js/amShopbyTopFilters',
-        }
-    },
-};
