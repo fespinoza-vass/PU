3ad10d4031d86733449ed7532548f44738cd527f
joseantonio.marquez <joseantonio.marquez@vasscompany.com>
2024-10-11 12:19:08 +0200

    Moving back to original Module

---
+++ app/design/frontend/Vass/PU/Amasty_Shopby/web/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            amShopbyApplyFilters: 'Amasty_Shopby/js/amShopbyApplyFilters',
+            amShopbyTopFilters: 'Amasty_Shopby/js/amShopbyTopFilters',
+        }
+    },
+};
--- app/design/frontend/Vass/PU/Amasty_ShopbyBase/web/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            amShopbyApplyFilters: 'Amasty_ShopbyBase/web/css/js/amShopbyApplyFilters',
-            amShopbyTopFilters: 'Amasty_ShopbyBase/web/css/js/amShopbyTopFilters',
-        }
-    },
-};
