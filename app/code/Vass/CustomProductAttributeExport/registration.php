b1d9879d433932682ea9843f753ee768562a8b9d
Victor Sandoval Cea <victor.sandoval@vasscompany.com>
2024-10-16 17:24:17 -0300

    Vass Custom products attribute export

---
+++ app/code/Vass/CustomProductAttributeExport/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Vass_CustomProductAttributeExport',
+    __DIR__
+);
